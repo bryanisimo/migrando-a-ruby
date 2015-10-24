02b4c1bdb6aafd1b6a4046a2afe0f4a5bbb37f30
Bryan <bryanisimo@gmail.com>
2015-10-24 10:49:48 -0500

    Validación con mensaje

--- clase4/workshop/app/models/book.rb
+++ clase4/workshop/app/models/book.rb
@@ -2,5 +2,9 @@ class Book < ActiveRecord::Base
   validates :title, presence: true
   validates :year,
     presence: true,
-    numericality: { only_integer: true }
+    # presence: {message: 'Debes ingresar el año'},
+    numericality: { only_integer: true, message: 'Debe ser un año' }
+  attr_accessor :custom
+  # attr_writer :custom
+  # attr_reader :custom
 end
